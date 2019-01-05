24fd16e7474213511f5aeba1eda172aca0918bd4
Main Desktop <robrousejr@gmail.com>
2019-01-04 21:52:30 -0500

    Added Input Activity

--- app/src/main/java/com/example/android/lifesim/StartActivity.java
+++ app/src/main/java/com/example/android/lifesim/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.lifesim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    // Button click starts input Activity
+    public void inputScreenActivity(View view) {
+        Intent intent = new Intent(this, inputActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/android/lifesim/inputActivity.java
@@ -0,0 +1,89 @@
+package com.example.android.lifesim;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class inputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+
+    public void beginMainGame(View view) {
+        Intent intent = new Intent(this, MainGame.class);
+
+        EditText firstName = (EditText)findViewById(R.id.firstName);
+        EditText lastName = (EditText)findViewById(R.id.lastName);
+        ToggleButton btnMale = (ToggleButton)findViewById(R.id.btnMale);
+        ToggleButton btnFemale = (ToggleButton)findViewById(R.id.btnFemale);
+        TextView errorView = (TextView)findViewById(R.id.errorTextBox);
+
+        String firstNameText = firstName.getText().toString();
+        String lastNameText = lastName.getText().toString();
+        String genderText = "";
+
+        /*     Error checking here     */
+        if(firstName.getText().toString().isEmpty() || lastName.getText().toString().isEmpty()){ // if either name is blank
+            errorView.setText("No characters in name");
+            return;
+        }
+        if(checkWhiteSpace(firstNameText) || checkWhiteSpace(lastNameText)){ // if there is whitespace
+            errorView.setText("No Spaces in Name");
+            return;
+        }
+        if(isNameOverTenChars(firstNameText) || isNameOverTenChars(lastNameText)){ // if names are over 10 characters
+            errorView.setText("Keep name under 10 characters");
+            return;
+        }
+        if((btnFemale.isChecked() && btnMale.isChecked()) || (!btnFemale.isChecked() && !btnMale.isChecked())){
+            errorView.setText("Choose one gender");
+            return;
+        }
+
+        if(btnMale.isChecked()){
+            genderText = "Male";
+        } else if (btnFemale.isChecked()){
+            genderText = "Female";
+        }
+
+        intent.putExtra("firstName", firstNameText);
+        intent.putExtra("lastName", lastNameText);
+        intent.putExtra("gender", genderText);
+
+        startActivity(intent);
+    }
+
+    // returns bool if string has any whitespace or not
+    public static boolean checkWhiteSpace(String testString){
+        if(testString != null){
+            for(int i = 0; i < testString.length(); i++){
+                if(Character.isWhitespace(testString.charAt(i))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isNameOverTenChars(String testString){
+        int len = 0;
+        if(testString != null){
+            for(int i = 0; i < testString.length(); i++){
+                len++;
+                if(i >= 10){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+}
