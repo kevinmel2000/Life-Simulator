86ac59b6835203bdd7ee2eb2c032c2644dfa3cfa
Main Desktop <robrousejr@gmail.com>
2019-01-04 22:04:17 -0500

    Loaded in data into MainGame

--- app/src/main/java/com/example/android/lifesim/MainGame.java
+++ app/src/main/java/com/example/android/lifesim/MainGame.java
@@ -1,7 +1,9 @@
 package com.example.android.lifesim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainGame extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        /* loads in data from inputActivity screen */
+        Intent intent = getIntent();
+        String genderText = intent.getStringExtra("gender");
+        String firstNameText = intent.getStringExtra("firstName");
+        String lastNameText = intent.getStringExtra("lastName");
+
+        TextView tmp = (TextView)findViewById(R.id.tmp);
+        tmp.setText(genderText + " " + firstNameText + " " + lastNameText);
+
+
     }
 }
