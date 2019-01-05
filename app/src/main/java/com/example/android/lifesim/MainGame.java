93879e703e4bcafef2219878bc13365a3d350042
Main Desktop <robrousejr@gmail.com>
2019-01-05 00:54:39 -0500

    Added output text in printFirstTextView func

--- app/src/main/java/com/example/android/lifesim/MainGame.java
+++ app/src/main/java/com/example/android/lifesim/MainGame.java
@@ -1,8 +1,11 @@
 package com.example.android.lifesim;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainGame extends AppCompatActivity {
@@ -23,6 +26,42 @@ public class MainGame extends AppCompatActivity {
         nameView.setText(firstNameText);
         nameViewLast.setText(lastNameText);
 
+        Person mainPerson = new Person(firstNameText + " " + lastNameText, 0);
+        printFirstTextView(mainPerson);
+
+    }
+
+    // Prints first TextView to the ScrollView
+    void printFirstTextView(Person mainPerson){
+
+        /* DYAMICALLY ADDS TEXVIEW TO SCROLLVIEW */
+        //create a TextView with Layout parameters according to your needs
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //if your parent Layout is relativeLayout, just change the word LinearLayout with RelativeLayout
+        final TextView tv = new TextView(this);
+        tv.setLayoutParams(lparams);
+        tv.setTextSize(15);
+        tv.setTextColor(Color.BLACK);
+        tv.setPadding(0,0,0,40);
+
+        /*Sets Drawable for border on bottom of textview in scrollview*/
+        final int sdk = android.os.Build.VERSION.SDK_INT;   // gets int version of os build
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) // if os is less than Jelly Bean then make it drawable
+        {
+            tv.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.maingametextviewborderbottom) );
+        } else {
+            tv.setBackground(ContextCompat.getDrawable(this, R.drawable.maingametextviewborderbottom));
+        }
+
+
+        // Set Text Here !!!!!!!!!!!!!!!
+        tv.setText(getString(R.string.InitialTextViewIntro, mainPerson.getName()));
+
+
+
+        //get the parent layout for your new TextView and add the new TextView to it
+        LinearLayout linearLayout = findViewById(R.id.insideScrollView);
+        linearLayout.addView(tv);
 
     }
 }
--- app/src/main/java/com/example/android/lifesim/Person.java
+++ app/src/main/java/com/example/android/lifesim/Person.java
@@ -1,5 +1,22 @@
 package com.example.android.lifesim;
 
 public class Person {
-    
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    /*GETTER FUNCTIONS*/
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
