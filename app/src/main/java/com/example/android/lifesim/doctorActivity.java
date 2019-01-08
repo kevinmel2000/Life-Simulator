53d3ee9d7e68f83bce8d7c92bc3c871f43597e96
Rob Rouse Jr <robrousejr@gmail.com>
2019-01-07 19:04:21 -0500

    Made Doctor Activity Popup transparent

--- app/src/main/java/com/example/android/lifesim/doctorActivity.java
+++ app/src/main/java/com/example/android/lifesim/doctorActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.lifesim;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class doctorActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class doctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        // makes popup window 70% by 30% of screen
+        getWindow().setLayout((int)(width * .8), (int)(height * .6));
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
     }
 }
